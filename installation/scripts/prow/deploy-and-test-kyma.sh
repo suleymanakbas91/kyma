275658b033d68e0bd22f80abff45eb91b8b2a65a
I332735 <suleyman.akbas@sap.com>
2018-12-18 11:53:31 +0100

    Move prow folder

---
+++ installation/scripts/prow/deploy-and-test-kyma.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+###
+# Following script deploys Kyma on Minikube without a VM driver, and runs the integrations tests. 
+#
+
+set -o errexit
+
+CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+SCRIPTS_DIR=${CURRENT_DIR}/../
+INSTALLATION_DIR=${CURRENT_DIR}/../../
+
+sudo ${INSTALLATION_DIR}/cmd/run.sh --vm-driver "none"
+sudo ${SCRIPTS_DIR}/is-installed.sh
+sudo ${SCRIPTS_DIR}/testing.sh
